f906fc7e3cc9792940581e8ae6bcc83393557b0a
Victor Nazario <33332105+victor-nazario@users.noreply.github.com>
2024-08-25 10:42:18 -0400

    Add users with roles

---
+++ internal/user/user.go
@@ -0,0 +1,28 @@
+package user
+
+// This is an oversimplification for challenge purposes, just to
+// provide the construct of a user with permissions
+
+type User struct {
+	UserName string   `json:"userName"`
+	Roles    []string `json:"roles"`
+}
+
+type Users map[string]User
+
+// ActiveUsers returns a list of all active users along with their current assigned roles
+func ActiveUsers() (Users, error) {
+	users := make(map[string]User)
+
+	users["operant"] = User{
+		UserName: "operant",
+		Roles:    []string{"cluster-owner"},
+	}
+
+	users["reader"] = User{
+		UserName: "reader",
+		Roles:    []string{"cluster-reader"},
+	}
+
+	return users, nil
+}
